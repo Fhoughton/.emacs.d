628cbc11d632e7dbd82b380d0cdf89aaa834ca98
Some Dude <fdphoughton@protonmail.com>
2025-02-08 14:30:51 +0000

    Add gitignore

--- elisp/theme.el
+++ elisp/theme.el
@@ -12,6 +12,30 @@
 (set-window-buffer nil (current-buffer)) ; Enable the new margins
 
 ;; Colours
+(set-face-attribute 'default nil
+		    :foreground "#37474F")
+
+(set-face-attribute 'region nil
+		    :background "#ECEFF1")
+
+(set-face-attribute 'font-lock-keyword-face nil
+		    :foreground "#673AB7")
+
+(set-face-attribute 'font-lock-builtin-face nil
+		    :foreground "#673AB7")
+
+(set-face-attribute 'font-lock-string-face nil
+		    :foreground "#FFAB91")
+
+(set-face-attribute 'font-lock-comment-face nil
+		    :foreground "#90A4AE")
+
+;; (set-face-attribute 'font-lock-function-name-face nil
+;; 		    :foreground "#263238")
+
+;; (set-face-attribute 'font-lock-variable-name-face nil
+;; 		    :foreground "#263238"
+;; 		    :weight 'bold)
 
 ;; Modeline
 (setq-default header-line-format mode-line-format) ; Make the header line contain the same information as the mode-line
